d2984d95d4954157fb2d4fc41578cd6624b94191
Ayush Bulbule <69710917+Ayush-Bulbule@users.noreply.github.com>
2022-08-08 09:51:10 +0530

    Add files via upload

---
+++ FC3410-GUIApplicationDevelopmentusing.NET/Microproject/studentinfo.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[studentinfo]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RegistrationNum] NCHAR(10) NULL, 
+    [FullName] NCHAR(80) NULL, 
+    [Gender] NCHAR(10) NULL, 
+    [Address] NCHAR(500) NULL, 
+    [Phone] NCHAR(15) NULL, 
+    [Email] NCHAR(100) NULL, 
+    [Stream] NCHAR(30) NULL, 
+    [Sem] NCHAR(20) NULL, 
+    [Age] NCHAR(10) NULL, 
+    [Picture] IMAGE NULL
+)
